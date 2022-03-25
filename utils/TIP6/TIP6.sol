133cf00bcc5e4921c169c04aa0aa44f27ed229cf
root <root@dev-nikita.local>
2022-03-25 14:06:14 +0000

    add bounce: false in getter

--- utils/TIP6/TIP6.sol
+++ utils/TIP6/TIP6.sol
@@ -11,7 +11,7 @@ abstract contract TIP6 is ITIP6 {
     mapping(bytes4 => bool) internal _supportedInterfaces;
 
     function supportsInterface(bytes4 interfaceID) external override view responsible returns (bool) {
-        return _supportedInterfaces[interfaceID];
+        return {value: 0, flag: 64, bounce: false} _supportedInterfaces[interfaceID];
     }
 
 }
